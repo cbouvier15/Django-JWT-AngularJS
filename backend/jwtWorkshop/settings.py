378a206745a7438d53521d73317e355bf0d806a1
Christian Bouvier <cbouvier@trea.uy>
2016-03-30 23:31:32 -0300

    Only JSONWebToken as Authentication class

--- backend/jwtWorkshop/settings.py
+++ backend/jwtWorkshop/settings.py
@@ -55,8 +55,6 @@ MIDDLEWARE_CLASSES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
 }
